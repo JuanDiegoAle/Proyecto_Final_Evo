5f41c63c5c8a88b08fdab7ccd3c0e305daa101b6
Juandiego <juanoriundo2017@gmail.com>
2026-07-02 09:21:28 -0500

    "feat: Agregar modelo e interfaz para gestion de clientes"

---
+++ ProyectoVentas/Interfaces/IClienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProyectoVentas.Models;
+using System.Collections.Generic;
+
+namespace ProyectoVentas.Interfaces
+{
+    public interface IClienteRepository
+    {
+        void Guardar(Cliente cliente);
+        void Actualizar(Cliente cliente);
+        void Eliminar(int id);
+        List<Cliente> ObtenerTodos();
+    }
+}
---
+++ ProyectoVentas/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoVentas.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string DNI { get; set; }
+        public string Telefono { get; set; }
+    }
+}
