c3d1b3c91aa985925cddbb2fdb4466d57c7c62b3
AlexPhytom <valexanderch@gmail.com>
2026-07-04 15:51:16 -0500

    feat:Commit Arreglo

--- ProyectoVentas/Repository/UsuarioRepository.cs
+++ ProyectoVentas/Repository/UsuarioRepository.cs
@@ -8,6 +8,46 @@ namespace ProyectoVentas.Repository
 {
     public class UsuarioRepository : IUsuarioRepository
     {
+        public bool Login(string username, string password)
+        {
+            using (SqlConnection conn = new SqlConnection(ConexionBD.Cadena))
+            {
+                conn.Open();
+
+                string query = @"
+         SELECT COUNT(*)
+         FROM Usuario
+         WHERE Username = @Username
+         AND Password = @Password";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                int count = (int)cmd.ExecuteScalar();
+
+                return count > 0;
+            }
+        }
+        public string ObtenerRol(string username)
+        {
+            using (SqlConnection conn = new SqlConnection(ConexionBD.Cadena))
+            {
+                conn.Open();
+
+                string query = @"
+         SELECT Rol
+         FROM Usuario
+         WHERE Username = @Username";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Username", username);
+
+                object result = cmd.ExecuteScalar();
+
+                return result != null ? result.ToString() : "";
+            }
+        }
         public void Guardar(Usuario u)
         {
             using (SqlConnection con = new SqlConnection(ConexionBD.Cadena))
