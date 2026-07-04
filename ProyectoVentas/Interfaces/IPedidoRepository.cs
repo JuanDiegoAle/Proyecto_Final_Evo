f62375a68a5b64fb44ac2f17cefff4eb2ffb1219
SantiagoMau <smauricioquine@gmail.com>
2026-07-04 18:13:21 -0500

    feat: Agregar metodos estadisticos a IPedidoRepository para el Dashboard

--- ProyectoVentas/Interfaces/IPedidoRepository.cs
+++ ProyectoVentas/Interfaces/IPedidoRepository.cs
@@ -17,7 +17,6 @@ namespace ProyectoVentas.Interfaces
         List<Pedido> FiltarPorMetodo(string metodo);
         decimal ObtenerTotalVendido();
 
-        decimal ObtenerTotalVendido();
         Dictionary<string, decimal> ObtenerVentasPorVendedor();
         Dictionary<string, int> ObtenerMetodosPagoEstadistica(); 
     }
