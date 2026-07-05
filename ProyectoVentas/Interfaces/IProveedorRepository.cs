a20cc8a8166f49aefd55454638abe51987c84986
jeanrmr43-sketch <jean.rmr43gmail.com>
2026-07-05 09:27:25 -0500

    "feat: Crear modelo Proveedor e interfaz IProveedorRepository"

---
+++ ProyectoVentas/Interfaces/IProveedorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoVentas.Models;
+
+namespace ProyectoVentas.Interfaces
+{
+    public interface IProveedorRepository
+    {
+        void Registrar(Proveedor proveedor);
+        void Eliminar(int id);
+        List<Proveedor> ObtenerTodos();
+    }
+}
---
+++ ProyectoVentas/Models/Proveedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoVentas.Models
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public string Ruc { get; set; }
+        public string RazonSocial { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
