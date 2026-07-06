99e3d08aa1454d61c718e3710af98f89a93a960c
jeanrmr43-sketch <jean.rmr43gmail.com>
2026-07-06 02:51:22 -0500

    "feat: Implementar CRUD en ProveedorRepository (Re-editado)"

--- ProyectoVentas/Repository/ProveedorRepository.cs
+++ ProyectoVentas/Repository/ProveedorRepository.cs
@@ -4,9 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ProyectoVentas.Interfaces;
+using ProyectoVentas.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
 namespace ProyectoVentas.Repository
 {
-    internal class ProveedorRepository
+    public class ProveedorRepository : IProveedorRepository
     {
+        public void Registrar(Proveedor p)
+        {
+            using (SqlConnection con = new SqlConnection(ConexionBD.Cadena))
+            {
+                con.Open();
+                string query = "INSERT INTO Proveedor (Ruc, RazonSocial, Telefono, Direccion) VALUES (@ruc, @razon, @tel, @dir)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@ruc", p.Ruc);
+                cmd.Parameters.AddWithValue("@razon", p.RazonSocial);
+                cmd.Parameters.AddWithValue("@tel", p.Telefono);
+                cmd.Parameters.AddWithValue("@dir", p.Direccion);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Eliminar(int id)
+        {
+            using (SqlConnection con = new SqlConnection(ConexionBD.Cadena))
+            {
+                con.Open();
+                string query = "DELETE FROM Proveedor WHERE Id=@id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public List<Proveedor> ObtenerTodos()
+        {
+            List<Proveedor> lista = new List<Proveedor>();
+            using (SqlConnection con = new SqlConnection(ConexionBD.Cadena))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Id, Ruc, RazonSocial, Telefono, Direccion FROM Proveedor", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    lista.Add(new Proveedor
+                    {
+                        Id = (int)dr["Id"],
+                        Ruc = dr["Ruc"].ToString(),
+                        RazonSocial = dr["RazonSocial"].ToString(),
+                        Telefono = dr["Telefono"].ToString(),
+                        Direccion = dr["Direccion"].ToString()
+                    });
+                }
+            }
+            return lista;
+        }
     }
 }
